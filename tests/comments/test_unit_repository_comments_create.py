7ce8e0a40911da3a7e0e4ca90a1c17fe172ed43c
Anastasia-Makarova <nastasia.makarova@gmail.com>
2024-03-28 20:38:58 +0100

    1_comments/ create

--- tests/comments/test_unit_repository_comments_create.py
+++ tests/comments/test_unit_repository_comments_create.py
@@ -16,16 +16,17 @@ from src.repository.comments import create_comment
 class TestAsyncComments(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self) -> None:
-        self.user = User(id=1, user_name='admin', password='admin')
-        self.session=AsyncMock(spec=AsyncSession)
-
+        self.user = User(id=1, user_name="admin", password="admin")
+        self.session = AsyncMock(spec=AsyncSession)
 
     async def test_create_comment_success(self):
-        body = CommentSchema(comment='test_comment', 
-                             created_at=datetime.now(), 
-                             updated_at=None, 
-                             photo_id=1, 
-                             created_by=self.user.id)
+        body = CommentSchema(
+            comment="test_comment",
+            created_at=datetime.now(),
+            updated_at=None,
+            photo_id=1,
+            created_by=self.user.id,
+        )
         result = await create_comment(body, 1, self.user, self.session)
         self.assertIsInstance(result, PhotoComment)
         self.assertEqual(result.comment, body.comment)
@@ -35,21 +36,17 @@ class TestAsyncComments(unittest.IsolatedAsyncioTestCase):
         self.session.commit.assert_called_once()
         self.session.refresh.assert_called_once_with(result)
 
-
-    
     async def test_create_comment_failure_missing_body(self):
         # Arrange
         body = None
         photo_id = 1
         created_by = 1
         db = AsyncMock()
-        
+
         # Act & Assert
         with self.assertRaises(AttributeError) as context:
             await create_comment(body, photo_id, created_by, db)
-        
-        self.assertEqual(str(context.exception), "'NoneType' object has no attribute 'comment'")
-        
 
-
-
+        self.assertEqual(
+            str(context.exception), "'NoneType' object has no attribute 'comment'"
+        )
