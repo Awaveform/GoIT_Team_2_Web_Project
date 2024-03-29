987dcef38aa06d118fda33423b0d8f34c74550a4
An82git <grugorchyk82@gmail.com>
2024-03-29 17:25:21 +0200

    1. POST - Rate photo

--- tests/tests_repository_rates/test__filter_by.py
+++ tests/tests_repository_rates/test__filter_by.py
@@ -19,13 +19,9 @@ class TestFilterBy(unittest.IsolatedAsyncioTestCase):
     
     async def test_valid_arguments(self):
         self.query.filter().filter.return_value = self.query
-        # self.query.filter = MagicMock()
-        # getattr = MagicMock()
 
         result = await _filter_by(query=self.query, **self.filter_by_data)
 
-        # self.query.filter.assert_any_call((Rate.photo_id == 1))
-        # self.query.filter.assert_any_call(Rate.created_by == 2)
         self.assertEqual(self.query, result)
 
     async def test_incorrect_argument(self):
