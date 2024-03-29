182772b620b31c567ac808b8ed34850367a77341
An82git <grugorchyk82@gmail.com>
2024-03-29 15:11:55 +0200

    1. POST - Rate photo

--- src/schemas.py
+++ src/schemas.py
@@ -49,7 +49,47 @@ class TokenModelResponse(BaseModel):
     token_type: str = "bearer"
 
 
-<<<<<<< HEAD
+class PhotoBase(BaseModel):
+    description: str | None = Field(max_length=500)
+
+
+class PhotoResponse(PhotoBase):
+    id: int
+    url: str
+    created_by: int
+    created_at: datetime
+
+
+class BaseTransformParamsModel(BaseModel):
+    effect: str | None
+    angle: PositiveInt | None
+    crop: str | None
+    gravity: str | None
+    width: PositiveInt | None
+    height: PositiveInt | None
+
+
+class TransformPhotoModel(BaseModel):
+    to_override: bool
+    description: str
+    params: BaseTransformParamsModel
+
+
+class TransformedPhotoModelResponse(BaseModel):
+    id: int
+    url: str
+    description: str
+    created_at: datetime
+    updated_at: datetime | None
+    created_by: int
+    updated_by: int | None
+    original_photo_id: int | None
+    is_transformed: bool | None
+
+    class Config:
+        orm_mode = True
+
+
 class RateModel(BaseModel):
     grade: int
 
@@ -57,90 +97,6 @@ class RateModel(BaseModel):
 class RateModelResponse(RateModel):
     created_at: datetime
     updated_at: datetime | None
-
-    class Config:
-        orm_mode = True
-=======
-class PhotoBase(BaseModel):
-    description: str | None = Field(max_length=500)
-
-
-class PhotoResponse(PhotoBase):
-    id: int
-    url: str
-    created_by: int
-    created_at: datetime
-
-
-
-class BaseTransformParamsModel(BaseModel):
-    effect: str | None
-    angle: PositiveInt | None
-    crop: str | None
-    gravity: str | None
-    width: PositiveInt | None
-    height: PositiveInt | None
-
-
-class TransformPhotoModel(BaseModel):
-    to_override: bool
-    description: str
-    params: BaseTransformParamsModel
-
-
-class TransformedPhotoModelResponse(BaseModel):
-    id: int
-    url: str
-    description: str
-    created_at: datetime
-    updated_at: datetime | None
-    created_by: int
-    updated_by: int | None
-    original_photo_id: int | None
-    is_transformed: bool | None
-
-    class Config:
-        orm_mode = True
->>>>>>> 898d1b1bda34cfae1ce8cf87a39f165624b80d84
-
-
-class PhotoBase(BaseModel):
-    description: str | None = Field(max_length=500)
-
-
-class PhotoResponse(PhotoBase):
-    id: int
-    url: str
-    created_by: int
-    created_at: datetime
-
-
-
-class BaseTransformParamsModel(BaseModel):
-    effect: str | None
-    angle: PositiveInt | None
-    crop: str | None
-    gravity: str | None
-    width: PositiveInt | None
-    height: PositiveInt | None
-
-
-class TransformPhotoModel(BaseModel):
-    to_override: bool
-    description: str
-    params: BaseTransformParamsModel
-
-
-class TransformedPhotoModelResponse(BaseModel):
-    id: int
-    url: str
-    description: str
-    created_at: datetime
-    updated_at: datetime | None
-    created_by: int
-    updated_by: int | None
-    original_photo_id: int | None
-    is_transformed: bool | None
 
     class Config:
         orm_mode = True
